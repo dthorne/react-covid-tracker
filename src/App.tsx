fdbd9276233a2e2eb1683fecd4204858f5d10e5f
Dewey McNeill <dmcneill@familysearch.org>
2021-02-05 10:33:49 -0700

    Creates RCT Tracking Card component
    Places 10 dummy cards on the main page to show it's working.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
-import RCTHeader from './components/RCTHeader/RCTHeader';
 import './App.css';
-import { DateContext, useDateContextValue } from './components/DatePicker/DatePickerContext';
+import RCTHeader from './components/RCTHeader/RCTHeader';
+import RCTTrackingCard from './components/RCTTrackingCard/RCTTrackingCard';
+import {DateContext, useDateContextValue} from './components/DatePicker/DatePickerContext';
 
 function App() {
-  const defaults = useDateContextValue();
+  const defaultDateContextValue = useDateContextValue();
+  const graphData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <DateContext.Provider value={defaults}>
+    <DateContext.Provider value={defaultDateContextValue}>
       <div className="App">
         <RCTHeader />
+        <div className="card-grid">
+          {graphData.map((data) => (
+            <RCTTrackingCard key={data} />
+          ))}
+        </div>
       </div>
     </DateContext.Provider>
   );
--- src/components/RCTTrackingCard/RCTTrackingCard.tsx
+++ src/components/RCTTrackingCard/RCTTrackingCard.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './RCTTrackingCard.css';
 
-const RCTTrackingCard: React.FC = () => (
-  <div className="RCTTrackingCard" data-testid="RCTTrackingCard">
-    RCTTrackingCard Component
-  </div>
-);
+const RCTTrackingCard: React.FC = () => {
+
+  return (
+    <div className="RCTTrackingCard card" data-testid="RCTTrackingCard">
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+        <code>TODO: Place graph here</code>
+      </div>
+    </div>
+  );
+}
 
 export default RCTTrackingCard;
