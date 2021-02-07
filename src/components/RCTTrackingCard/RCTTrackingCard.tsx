73645506720415666801d65bafef44268f641a2c
Dewey McNeill <dmcneill@familysearch.org>
2021-02-06 23:59:54 -0700

    Fixes issue with date format in api requests

--- src/components/RCTTrackingCard/RCTTrackingCard.tsx
+++ src/components/RCTTrackingCard/RCTTrackingCard.tsx
@@ -1,6 +1,7 @@
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React, {useState} from 'react';
+import moment from 'moment';
 import {ChartData, ChartDataSets} from 'chart.js';
 import {Bar, Line} from 'react-chartjs-2';
 import {Card, CardListContext, ChartType} from '../CardListContext/CardListContext';
@@ -8,21 +9,23 @@ import {DateContext} from '../DatePicker/DatePickerContext';
 import './RCTTrackingCard.css';
 
 function buildCovid19ApiUrl(startDate: Date | null, endDate: Date | null, country: string) {
-  return `https://api.covid19api.com/country/${country}/status/confirmed?from=${getDateString(startDate)}&to=${getDateString(endDate)}`
+  return `https://api.covid19api.com/country/${country}/status/confirmed?from=${getDateString(startDate)}T00:00:00Z&to=${getDateString(endDate)}T00:00:00Z`
 }
 
 function getDateString(date: Date | null): string {
   if (!date) return ``;
-  return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+  return moment(date).format('YYYY-MM-DD');
 }
 
 const colors = ['#003844', '#006C67', '#42E2B8', '#FFB100', '#FFEBC6'];
-
 function buildDataset(data: DataPoint[], index: number): ChartDataSets {
+  const color = colors[index % colors.length];
   return {
     label: data[0].Country,
     fill: false,
-    borderColor: colors[index % colors.length], 
+    borderColor: color, 
+    backgroundColor: color,
+    borderWidth: 1,
     data: data.map(d => d.Cases)
   }
 }
@@ -70,9 +73,8 @@ const RCTTrackingCard: React.FC<TrackingCardProps> = ({id, card: {title, type, c
           <div>{title}</div>
           <FontAwesomeIcon icon={faTrash} onClick={handleRemoveCard} />
         </h5>
-        <h6 className="card-subtitle mb-2 text-muted">{ countries.map(m => m.Country).join(' & ') }</h6>
-        {chartData && type === ChartType.Line && <Line data={chartData as any} />}
-        {chartData && type === ChartType.Bar && <Bar data={chartData as any} />}
+        {chartData && type == ChartType.Line && <Line data={chartData as any} />}
+        {chartData && type == ChartType.Bar && <Bar data={chartData as any} />}
       </div>
     </div>
   );
